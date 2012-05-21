ee4fc2046715d5d9578bd29558cdcbaaa5afbdb2
NuclearW <incongruency@gmail.com>
2012-05-21 01:05:31 -0400

    Some more logging

--- src/main/java/com/gmail/nossr50/SpawnPlugin.java
+++ src/main/java/com/gmail/nossr50/SpawnPlugin.java
@@ -13,27 +13,31 @@ import com.gmail.nossr50.listeners.PlayerListener;
 import com.gmail.nossr50.runnables.TeleportHandler;
 
 public class SpawnPlugin extends JavaPlugin {
-    
+
     public HashMap<Player, Long> waitingToTeleport = new HashMap<Player, Long>();
     public String infoPrefix = ChatColor.GOLD+"[vSpawn] "+ChatColor.RED;
-    
+
     PlayerListener playerListener = null;
     EntityListener entityListener = null;
-    
+
     public void onEnable() {
         playerListener = new PlayerListener(this);
         entityListener = new EntityListener(this);
-        
+
         Bukkit.getPluginManager().registerEvents(playerListener, this);
         Bukkit.getPluginManager().registerEvents(entityListener, this);
-        
+
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new TeleportHandler(this), 0, 20);
-        
+
         getCommand("spawn").setExecutor(new Spawn(this));
+
+        getLogger().info("Finished Loading " + getDescription().getFullName());
     }
     
     public void onDisable() {
         Bukkit.getScheduler().cancelTasks(this);
+
+        getLogger().info("Finished Unloading "+getDescription().getFullName());
     }
 
 }
